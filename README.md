428014ecb57a57732d628f1c40c229d763b742b6
Baylak <mongush.baylak17@gmail.com>
2023-04-27 19:25:55 +0300

    Addd project

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# certificate_generation
+## Certificate Generator
+
+<!---Пример кода-->
+
+This is a simple script that generates certificates based on a given template for each student in a `Google Sheets` spreadsheet. The generated certificates are then uploaded to `Google Drive` and their links are added to the spreadsheet.
+
+### Installation
+1. Clone the repository to your local machine.
+2. Install the required Python packages using `pip install -r requirements.txt`.
+3. Set up the necessary authentication files:
+   * Create a `env` directory in the project root.
+   * Download the service account JSON file and save it as `service_account.json` in the `env` directory.
+   * Download the `client_secrets.json` file for Google Drive API authentication and save it in the `env` directory.
+4. Set up the `Google Sheets` document:
+   * Create a new `Google Sheets` document.
+   * Add a sheet to the document.
+   * In the first row of the sheet, add the following column headers: `Name`, `Surname`, `Certificate Link`, and `Referer`.
+5. Customize the certificate template:
+   * Create a new HTML file in the `templates` directory.
+   * Use Jinja2 templating syntax to customize the certificate template.
+6. Update the `folder_id` in the `upload_certificate_to_drive` function to match the ID of the folder in your Google Drive where you want the generated certificates to be uploaded.
+### Usage
+#### To run the script, execute the following command:
+>bash
+```
+python certificate_generator.py -t <path_to_template_file> -r <referer> -d <date>
+```
+Where `<path_to_template_file>` is the path to the HTML file containing the certificate template, and `<referer>` is the name of the person or organization issuing the certificates.
+
+The script will generate a certificate for each student in the `Google Sheets` document, upload the certificate to `Google Drive`, and add the link to the `Certificate Link` column of the corresponding row in the spreadsheet. The Referer column will also be updated with the name of the person or organization issuing the certificates.
+
+### License
+This project is licensed under the terms of the MIT license.
---
+++ certgenerator.py
@@ -0,0 +1,117 @@
+import argparse
+import os
+import uuid
+
+import gspread
+import jinja2
+import pdfkit
+from oauth2client.service_account import ServiceAccountCredentials
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Template')
+    parser.add_argument('-t', '--template', help='Template', required=True)
+    parser.add_argument('-r', '--referer', help='Referer', required=True)
+    parser.add_argument('-d', '--date', help='Date', required=True)
+
+    return parser.parse_args()
+
+
+def get_google_sheets_client():
+    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('env/service_account.json', scope)
+    return gspread.authorize(creds)
+
+
+def get_students_list(sheet):
+    students_list = sheet.get_all_values()
+
+    for i, row in enumerate(students_list):
+        for j, cell in enumerate(row):
+            if j == 0:
+                students_list[i] = {'name': cell, 'surname': row[j + 1], 'coordinates': (i + 1, j + 3)}
+
+    return students_list
+
+
+def generate_certificate(template, student, date):
+    context = {
+        "student_surname": student.get('surname'),
+        "student_name": student.get('name'),
+        "date": date,
+        "image_folder": os.path.join(os.path.abspath(os.path.dirname(__name__)), 'templates', 'images'),
+        "css_folder": os.path.join(os.path.abspath(os.path.dirname(__name__)), 'templates', 'css'),
+    }
+
+    rendered_template = template.render(context)
+    file_name = f"certificate-{uuid.uuid4()}"
+    options = {
+        "enable-local-file-access": None,
+        'margin-top': '0mm',
+        'margin-right': '0mm',
+        'margin-bottom': '0mm',
+        'margin-left': '0mm'
+    }
+
+    pdfkit.from_string(rendered_template, f"Certificates/{file_name}.pdf", options=options)
+    return file_name
+
+
+def upload_certificate_to_drive(drive, folder_id, file_name):
+    file = drive.CreateFile({'title': file_name, 'parents': [{'id': folder_id}]})
+    file.SetContentFile(f'Certificates/{file_name}.pdf')
+    file.Upload()
+
+    permission = file.InsertPermission({
+        'type': 'anyone',
+        'value': None,
+        'role': 'reader',
+        'withLink': True
+    })
+
+    return file['alternateLink']
+
+
+def update_google_sheet(sheet, student, referer, link):
+    sheet.update_cell(student.get('coordinates')[0], student.get('coordinates')[1] + 3, link)
+    sheet.update_cell(student.get('coordinates')[0], student.get('coordinates')[1] + 4, referer)
+
+
+def main():
+    args = parse_arguments()
+
+    # Authenticate Google API
+    gauth = GoogleAuth()
+    gauth.LocalWebserverAuth()
+    drive = GoogleDrive(gauth)
+
+    # Get Google Sheets client
+    sheets_client = get_google_sheets_client()
+
+    # Get template
+    with open(args.template, 'r') as f:
+        template_string = f.read()
+
+    template = jinja2.Template(template_string)
+
+    # Open spreadsheet
+    sheet = sheets_client.open('Students').sheet1
+
+    # Get students list
+    students_list = get_students_list(sheet)
+
+    date = args.date
+
+    # Generate and upload certificates
+    for student in students_list:
+        file_name = generate_certificate(template, student, date)
+        link = upload_certificate_to_drive(drive, 'folder_id', file_name)
+        update_google_sheet(sheet, student, args.referer, link)
+
+    print('Certificates generation and upload to Google Drive completed successfully!')
+
+
+if __name__ == '__main__':
+    main()
